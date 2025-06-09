9e3ea530bdfc62cc3b1bdc03dd935394091e5042
ifra abdi <ifraha461@gmail.com>
2025-06-09 21:59:19 +0300

    Complete

--- index.js
+++ index.js
@@ -1,61 +1,17 @@
-function example() {
-  console.log("This is a function declaration");
-}
-hoisted(); // works!
-function hoisted() {
-  console.log("I'm hoisted!");
-}
-const example = function () {
-  console.log("This is a function expression");
-};
-// A function without a name
-function () {
-  console.log("Anonymous function");
-}
-// Only valid inside an expression (e.g. callback, IIFE, etc.)
-// A function without a name
-function () {
-  console.log("Anonymous function");
-}
-// Only valid inside an expression (e.g. callback, IIFE, etc.)
-const sayHello = function () {
-  console.log("Hello!");
-};
-(function () {
-  console.log("IIFE ran immediately!");
-})();
-function outer() {
-  const message = "Hello";
-  function inner() {
-    console.log(message);
-  }
-  inner(); // can access message
-}
-function outer() {
-  const outerVar = "Outer";
-  return function inner() {
-    console.log(outerVar);
-  };
-}
-const innerFunc = outer();
-innerFunc(); // Outer
-function makeAdder(x) {
-  return function(y) {
-    return x + y;
-  };
-}
-const add5 = makeAdder(5);
-console.log(add5(3)); // 8
+// 1. Function Declaration with Default Parameter
 function saturdayFun(activity = "roller-skate") {
   return `This Saturday, I want to ${activity}!`;
 }
+
+// 2. Function Expression with Default Parameter
 const mondayWork = function(activity = "go to the office") {
   return `This Monday, I will ${activity}.`;
 };
+
+// 3. Function Returning a Function (Closure)
 function wrapAdjective(flair = "*") {
   return function(adjective = "special") {
     return `You are ${flair}${adjective}${flair}!`;
   };
 }
-wrapAdjective("!!!")("a hard worker");
-// => "You are !!!a hard worker!!!"
+ 
